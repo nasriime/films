f4572c4e4e1f684d67a43023aa7b8656ab138c12
nasriime <mnasr4040@gmail.com>
2022-01-14 18:11:09 +0100

    remove context

--- src/App.js
+++ src/App.js
@@ -6,18 +6,15 @@ import {
 import './App.scss';
 import List from "./components/List/List";
 import Details from './components/Details/Details';
-import MainContextProvider from './contexts';
 
 function App() {
   return (
-    <MainContextProvider>
-       <BrowserRouter>
-          <Routes>
-              <Route path="/" element={<List />} />
-              <Route path="/details" element={<Details />} />
-          </Routes>
-        </BrowserRouter>
-    </MainContextProvider>
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<List />} />
+          <Route path="/details" element={<Details />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
