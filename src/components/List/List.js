84a7d0d158f6d294f25b2683a9b211e16451121b
nasriime <mnasr4040@gmail.com>
2022-01-14 12:53:18 +0100

    setup search and filter

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,8 +1,10 @@
-import React, {useEffect, useState}  from 'react'
+import React, {useEffect, useState, useCallback}  from 'react'
 import Item from "../Item/Item";
+import { debounce } from '../../utils';
 
 const List = ()=> {
     const [data, setData] = useState([]);
+    const [order, setOrder] = useState('DESC');
 
 
     useEffect(() => {
@@ -27,13 +29,36 @@ const List = ()=> {
             )
     }, []);
 
+    
+
     const _onChange=(e)=>{
         console.log('e.target.value', e.target.value)
     }
 
+    const debouncedChangeHandler = useCallback(
+        debounce(_onChange, 300)
+    , []);
+
     return (
         <div>
-            <input type="text" onChange={(e)=>{_onChange(e)}}/> 
+            <input type="text" onChange={debouncedChangeHandler}/> 
+            <div className="ordering--btns">
+                <span>Filter by film title:</span>
+                <button 
+                    type="button" 
+                    className="btn btn-light" 
+                    onClick={()=> setOrder('DESC')} 
+                    disabled={order === 'DESC'}>
+                        DESC
+                </button>
+                <button 
+                    type="button" 
+                    className="btn btn-light" 
+                    onClick={()=> setOrder('ASC')} 
+                    disabled={order === 'ASC'}>
+                    ASC
+                </button>
+            </div>
              {data.length > 0 && data.map(item=>(
                 <Item key={item.id} item={item} />
                 )
